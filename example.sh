58b97b3bb086ef644e39a3dbcefe721f8b251218
stefan <katakombi@gmail.com>
2013-12-08 07:26:52 +0100

    updated example to include model compression

--- example.sh
+++ example.sh
@@ -1,27 +1,16 @@
 #!/bin/bash
 
 #This is simple example how to use rnnlm tool for training and testing rnn-based language models
-#Check 'example.output' how the output should look like
-#SRILM toolkit must be installed for combination with ngram model to work properly
 
 make clean
 make
 
 rm model
+rm model.compressed
 rm model.output.txt
 
 #rnn model is trained here
-time ./rnnlm -train train -valid valid -rnnlm model -hidden 15 -rand-seed 1 -debug 2 -class 100 -bptt 0 -bptt-block 10 -direct-order 3 -alpha 0.1
-
-# remove next line if you got SRILM installed!
-exit
-
-#ngram model is trained here, using SRILM tools
-ngram-count -text train -order 5 -lm templm -kndiscount -interpolate -gt3min 1 -gt4min 1
-ngram -lm templm -order 5 -ppl test -debug 2 > temp.ppl
-
-gcc convert.c -O2 -o convert
-./convert <temp.ppl >srilm.txt
-
-#combination of both models is performed here
-time ./rnnlm -rnnlm model -test test -lm-prob srilm.txt -lambda 0.5
+time ./rnnlm -train train -valid valid -rnnlm model -hidden 15 -rand-seed 1 -debug 2 -class 100 -bptt 4 -bptt-block 10 -direct-order 3 -alpha 0.1 -direct 35
+time ./rnnlm -rnnlm model -compress 4 -kmean 15 -write-compressed model.compressed -test test
+./rnnlm -rnnlm model.compressed -test test
+./rnnlm -rnnlm model -test test
