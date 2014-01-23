34c518a3adc7466026db22c1c3668300868ef9b4
katakombi <katakombi@gmail.com>
2014-01-23 16:11:37 +0100

    slightly changing example to make it run faster

--- example.sh
+++ example.sh
@@ -10,7 +10,7 @@ rm model.compressed
 rm model.output.txt
 
 #rnn model is trained here
-time ./rnnlm -train train -valid valid -rnnlm model -hidden 15 -rand-seed 1 -debug 2 -class 100 -bptt 4 -bptt-block 10 -direct-order 3 -alpha 0.1 -direct 35
-time ./rnnlm -rnnlm model -compress 4 -kmean 15 -write-compressed model.compressed -test test
+time ./rnnlm -train train -valid valid -rnnlm model -hidden 15 -rand-seed 1 -debug 2 -class 100 -bptt 4 -bptt-block 10 -direct-order 2 -alpha 0.1 -direct 5
+time ./rnnlm -rnnlm model -compress 4 -kmean 20 -write-compressed model.compressed -test test
 ./rnnlm -rnnlm model.compressed -test test
 ./rnnlm -rnnlm model -test test
